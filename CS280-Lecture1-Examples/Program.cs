7bfd1ae3549c39a0c413b5c438e6785b149a2edc
Shung0301 <tylertangbu@gmail.com>
2017-03-03 20:42:25 +0800

    加入了Hello World字串

--- CS280-Lecture1-Examples/Program.cs
+++ CS280-Lecture1-Examples/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace CS280_Lecture1_Examples
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
